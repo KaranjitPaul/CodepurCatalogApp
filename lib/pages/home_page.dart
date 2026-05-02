b55efec60542d030773f63a126843b6115800b31
KaranjitPaul <karanjitpaul1112@gmail.com>
2026-05-03 02:18:40 +0530

    Day 15 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -42,14 +42,41 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
-            ? ListView.builder(
-                itemCount:
-                    CatalogModel.items?.length, //CatalogModel.items.length
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 15,
+                  crossAxisSpacing: 15,
+                ),
                 itemBuilder: (context, index) {
-                  return ItemWidget(
-                    item: CatalogModel.items![index],
-                  ); //CatalogModel.items[index]
+                  final item = CatalogModel.items![index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: GridTile(
+                      header: Container(
+                        decoration: BoxDecoration(color: Colors.deepPurple),
+                        padding: const EdgeInsets.all(12),
+                        child: Text(
+                          item.name,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      footer: Container(
+                        decoration: BoxDecoration(color: Colors.black),
+                        padding: const EdgeInsets.all(12),
+                        child: Text(
+                          "\$${item.price.toString()}",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      child: Image.network(item.image),
+                    ),
+                  );
                 },
+                itemCount: CatalogModel.items?.length,
               )
             : Center(child: CircularProgressIndicator()),
       ),
